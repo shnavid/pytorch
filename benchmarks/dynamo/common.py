ee51f0abfd23de80413b29aa244bbe3c59d1dd11
Simon Fan <xmfan@meta.com>
2023-09-18 12:45:24 -0700

    only init distributed if args.only is present (leaf runs only)

--- benchmarks/dynamo/common.py
+++ benchmarks/dynamo/common.py
@@ -2971,7 +2971,7 @@ def parse_args(args=None):
 def process_entry(rank, runner, original_dir, args):
     args.rank = rank
     with maybe_init_distributed(
-        args.multiprocess,
+        args.use_distributed,
         rank=rank,
         world_size=args.world_size,
         port=args.distributed_master_port,
@@ -3006,6 +3006,7 @@ def main(runner, original_dir=None):
     if args.multiprocess_models_only:
         args.multiprocess = True
 
+    args.use_distributed = args.multiprocess and args.only
     if args.multiprocess:
         # NB: Do NOT query device count before CUDA initialization; we're
         # going to overwrite CUDA_VISIBLE_DEVICES and this will result in
